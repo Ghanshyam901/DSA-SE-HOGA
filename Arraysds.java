d44e2cc162f284cabe6f9a480e13f9bcb1343ab7
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-11-21 00:40:04 +0530

    90*rev

--- Arraysds.java
+++ Arraysds.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 class Arraysds{
@@ -89,4 +91,47 @@ class Arraysds{
                 matrix[i][n-1-j] = temp;
             }
         }
-}
+
+
+
+        //https://leetcode.com/problems/spiral-matrix/submissions/
+
+        public List<Integer> spiralOrder(int[][] matrix) {
+        
+            List<Integer> list = new ArrayList<>();
+            int row = matrix.length;
+            int col = matrix[0].length;
+            
+            int left =0;
+            int right =col-1;
+            int top =0;
+            int bottom = row-1;
+            
+            while(left <= right && top <=bottom){
+                
+                for(int i = left; i<=right; i++){
+                    list.add(matrix[top][i]);
+                }
+                top++;
+                
+                for(int i = top; i<=bottom; i++){
+                    list.add(matrix[i][right]);
+                }
+                right--;
+                
+                if(top <= bottom){
+                    for(int i=right; i>=left; i--){
+                        list.add(matrix[bottom][i]);
+                    }
+                    bottom--;
+                }
+                if(left <= right){
+                    for(int i=bottom; i>=top; i--){
+                        list.add(matrix[i][left]);
+                    }
+                    left++;
+                }
+            }
+            return list;
+        }
+    }
