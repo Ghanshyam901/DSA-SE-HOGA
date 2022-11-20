96a5ef1373d83518d3cca7b28b3c7f15da49f577
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-11-21 00:12:27 +0530

    90*rev

--- Arraysds.java
+++ Arraysds.java
@@ -63,4 +63,30 @@ class Arraysds{
         
                 
     }
+
+    //  https://leetcode.com/problems/rotate-image/submissions/
+
+    public void rotate(int[][] matrix) {
+        
+        // transpose
+        for(int i=0; i<matrix.length; i++){
+            for(int j =i; j<matrix[0].length; j++){
+                // int temp = 0;
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // reverse each row
+        int n = matrix.length;
+        
+        for(int i=0; i<matrix.length; i++)// transpose{
+            for(int j=0; j<matrix.length/2; j++){
+                 int temp=0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
 }
