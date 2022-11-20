76ba616d3ba21e21370eaf37cbb480eadfb5e483
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-11-20 23:49:17 +0530

    matrix

--- Arraysds.java
+++ Arraysds.java
@@ -32,4 +32,35 @@ class Arraysds{
         return ans;
         
     }
+
+    // https://leetcode.com/problems/set-matrix-zeroes/submissions/
+    public void setZeroes(int[][] matrix) {
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int [] dr = new int [n];
+        int [] dc = new int[m];
+        // Arrays.fill(dr,-1);
+        // Arrays.fill(dc,-1);
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    dr[i] = -1;
+                    dc[j] = -1;
+                }
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(dr[i] == -1 || dc[j] == -1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        
+                
+    }
 }
