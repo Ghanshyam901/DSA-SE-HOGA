ff0e42f185a124b1a35badeb9336f066a9c17057
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-04 23:15:44 +0530

    code

--- DailyLeetcode.java
+++ DailyLeetcode.java
@@ -2,4 +2,32 @@ public class DailyLeetcode {
     public static void main(String[] args) {
         
     }
+  /// 4-12-22  
+    public int minimumAverageDifference(int[] nums) {
+  
+        int n = nums.length;
+        long sum =0;
+        for(int i=0; i<nums.length; i++){
+            sum+=nums[i];
+           
+        }
+        
+        int idx  = 0;
+        long minabs = Integer.MAX_VALUE;
+        long psum = 0;
+        
+        for(int i=0; i<n; i++){
+            psum +=nums[i];
+            long left = psum/(i+1);
+            long right = i+1 == n ? 0 : (sum-psum)/(n-i-1);
+            long diff = Math.abs(left-right);
+            if(diff < minabs){
+                minabs = diff;
+                idx = i;
+            }
+        }
+        
+        return idx;
+    }
+
 }
