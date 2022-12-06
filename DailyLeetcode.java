cf14b4208188970891db9d780c0ff5a961dfe926
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-06 15:50:21 +0530

    leetcode challange

--- DailyLeetcode.java
+++ DailyLeetcode.java
@@ -1,3 +1,11 @@
+class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
 public class DailyLeetcode {
     public static void main(String[] args) {
         
@@ -28,6 +36,40 @@ public class DailyLeetcode {
         }
         
         return idx;
+    }
+
+    // 6-12-22
+
+    public ListNode oddEvenList(ListNode head) {
+        
+        ListNode dummyodd = new ListNode(-1);
+        ListNode evenNode = new ListNode(-1);
+
+        ListNode oddTail = dummyodd;
+        ListNode evenTail = evenNode;
+
+        ListNode curr = head;
+        int i = 1;
+
+        while(curr != null){
+            if(i % 2 != 0){
+                oddTail.next = curr;
+                oddTail = oddTail.next;
+            }else{
+                evenTail.next = curr;
+                evenTail = evenTail.next;
+            }
+            curr = curr.next;
+            i++;
+        }
+
+        oddTail.next = null;
+        evenTail.next = null;
+        oddTail.next = evenNode.next;
+
+    return dummyodd.next;
+
+
     }
 
 }
